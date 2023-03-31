de055c37c42e661e15e4ce1e291b8624c277c821
WillyLinl <109008271+WillyLinl@users.noreply.github.com>
2023-03-31 14:54:28 +0800

    Update urls.py

--- myreport/mysite/wordsapp/urls.py
+++ myreport/mysite/wordsapp/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('update_profile/', views.update_profile, name='update_profile'),
-    path('change_password/', views.change_password, name='change_password'),
-    path('search_company/', views.search_company, name='search_company'),
-    path('search_horoscope/', views.search_horoscope, name='search_horoscope'),
+    path('查詢/', views.查詢, name='查詢'),
+    path('birthday/', views.birthday, name='birthday'),
 ]
+
